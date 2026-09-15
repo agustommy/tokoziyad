bb35c6e05f80adc8abd0ed49617fe966f71c70ba
agustommy <agustommy77@gmail.com>
2026-09-15 12:34:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kasir-cache-v1';
+const CACHE_NAME = 'kasir-cache-v2';
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(['./']))
